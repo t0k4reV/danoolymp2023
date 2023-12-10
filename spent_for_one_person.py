dcae57a2ecde02ffa913b1ab2a741ec036d7958f
brokeboy <ras0087@ya.ru>
2023-12-10 16:08:09 +0300

    количество потраченных средств на одного человека в каждом городе

--- spent_for_one_person.py
+++ spent_for_one_person.py
@@ -1,35 +1,25 @@
 import pandas as pd
-
-# Загрузка данных о заказах игр
-games_orders = pd.read_csv('users_with_population.csv')
-
-# Здесь предполагается, что у вас уже есть столбец с численностью населения в каждом городе
-# Добавим столбец с общей суммой потраченных денег на игры
-games_orders['total_spent'] = games_orders['good_price'] * games_orders['good_cnt']
-
-# Группировка по городу и вычисление общей потраченной суммы на игры в каждом городе
-total_spent_per_city = games_orders.groupby('city_nm')['total_spent'].sum()
-
-# Объединение данных о потраченной сумме на игры и численности населения по городам
-data = pd.merge(total_spent_per_city, games_orders[['city_nm', 'city_population']], on='city_nm')
-
-# Вычисление среднего количества денег, потраченного на игры на душу населения
-data['spent_per_person'] = data['total_spent'] / data['city_population']
-
-# Вывод первых строк для проверки
-print(data.head())
-
 import matplotlib.pyplot as plt
 
-# Сортировка данных для более наглядного отображения
-data = data.sort_values('spent_per_person', ascending=False)
+# Предположим, что у вас есть DataFrame с данными, давайте назовем его df
+df = pd.read_csv('users_with_population.csv')
+# Группировка данных по городам и вычисление общей суммы потраченных денег на игры в каждом городе
+total_spent_by_city = df.groupby('city_nm')['good_price'].sum()
+print(total_spent_by_city)
+
+# Получение численности населения городов
+city_population = df.groupby('city_nm')['city_population'].max()
+print(city_population)
+
+# Расчет суммы на душу населения
+spent_per_capita = total_spent_by_city / city_population
 
 # Построение графика
 plt.figure(figsize=(10, 6))
-plt.bar(data['city_nm'], data['spent_per_person'])
+spent_per_capita.sort_values().plot(kind='bar', color='skyblue')
+plt.title('Средняя сумма потраченных денег на игры на душу населения в каждом городе')
 plt.xlabel('Город')
-plt.ylabel('Средние затраты на игры на душу населения')
-plt.title('Средние затраты на игры на душу населения в каждом городе')
-plt.xticks(rotation=45, ha='right')
+plt.ylabel('Средняя сумма потраченных денег на игры на душу населения')
+plt.xticks(rotation=90)
 plt.tight_layout()
 plt.show()
