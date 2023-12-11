134614aa256177261b33b07fb03a3bf6fc7970a6
brokeboy <ras0087@ya.ru>
2023-12-11 12:12:02 +0300

    spent + coordinates

--- spent_for_one_person.py
+++ spent_for_one_person.py
@@ -12,17 +12,17 @@ city_population = df.groupby('city_nm')['city_population'].max()
 # Расчет суммы на душу населения
 spent_per_capita = total_spent_by_city / city_population
 
-# Построение графика
-# plt.figure(figsize=(10, 6))
-# spent_per_capita.sort_values().plot(kind='bar', color='skyblue')
-# plt.title('Средняя сумма потраченных денег на игры на душу населения в каждом городе')
-# plt.xlabel('Город')
-# plt.ylabel('Средняя сумма потраченных денег на игры на душу населения')
-# plt.xticks(rotation=90)
-# plt.tight_layout()
-# plt.show()
+#Построение графика
+plt.figure(figsize=(10, 6))
+spent_per_capita.sort_values().plot(kind='bar', color='skyblue')
+plt.title('Средняя сумма потраченных денег на игры на душу населения в каждом городе')
+plt.xlabel('Город')
+plt.ylabel('Средняя сумма потраченных денег на игры на душу населения')
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
 
 ## Тут мы создаем отдельную таблицу с результатом
-print(spent_per_capita)
-spent_per_capita.to_csv('spent_per_capita.csv')
+# print(spent_per_capita)
+# spent_per_capita.to_csv('spent_per_capita.csv')
 
